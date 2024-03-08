b8d228bb20d95cd0c3925c9e04fa91c8876e3cad
kichu12348 <rmahadevan574@gmail.com>
2024-03-08 12:32:45 +0530

    added clusters

--- index.js
+++ index.js
@@ -1,57 +1,72 @@
+const cluster = require('cluster');
+const os = require('os');
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/user');
 const messageRoutes = require('./routes/message');
 require('dotenv').config();
 
-
 //middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 
 //mongoDB connection
-const {connect} = require('./services/connect');
+const { connect } = require('./services/connect');
 connect(process.env.MONGO_URL);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-//routes
-app.use('/user', userRoutes);
-app.use('/message', messageRoutes);
+// Check if the current process is the master
+if (cluster.isMaster) {
+  // Create worker processes based on the number of available CPU cores
+  const numCPUs = os.cpus().length;
+  console.log(`Master cluster setting up ${numCPUs} workers...`);
 
-app.get('/', (req, res) => {
-    
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  // Listen for dying workers and replace them
+  cluster.on('exit', (worker) => {
+    console.log(`Worker ${worker.id} died. Restarting...`);
+    cluster.fork();
+  });
+} else {
+  // Worker processes will share the same port
+  // routes
+  app.use('/user', userRoutes);
+  app.use('/message', messageRoutes);
+
+  app.get('/', (req, res) => {
     res.json({
-        message: 'Welcome to the server',
-    })
+      message: 'Welcome to the server',
+    });
+  });
 
-});
+  const server = app.listen(PORT, () => {
+    console.log(`Worker ${cluster.worker.id} is running on port ${PORT}`);
+  });
 
-const server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
-
-//socket.io
-const io = require('socket.io')(server,{
+  // socket.io
+  const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: '*',
-    }
-});
+      origin: '*',
+    },
+  });
 
-io.on('connection', (socket) => {
-    
-    socket.on('setup',(userData)=>{
-        socket.join(userData)
-        socket.emit('connected')
-    })
+  io.on('connection', (socket) => {
+    socket.on('setup', (userData) => {
+      socket.join(userData);
+      socket.emit('connected');
+    });
 
     socket.on('joinRoom', (roomID) => {
-        socket.join(roomID);
+      socket.join(roomID);
     });
 
     socket.on('sendMessage', (message) => {
-        io.to(message.roomID).emit('message', message);
+      io.to(message.roomID).emit('message', message);
     });
-});
+  });
+}
